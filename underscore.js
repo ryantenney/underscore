2607f84745d9c24fda59c56501ce0815e02e687a
Ryan W Tenney <ryan@10e.us>
2010-08-28 17:25:03 -0400

    Added 2nd parameter to `uniqueId`, `moreUnique` which, when truthy, uses the trailing digits from `Math.random()` as the id, instead of a counter.

--- underscore.js
+++ underscore.js
@@ -609,9 +609,9 @@
   // Generate a unique integer id (unique within the entire client session).
   // Useful for temporary DOM ids.
   var idCounter = 0;
-  _.uniqueId = function(prefix) {
-    var id = idCounter++;
-    return prefix ? prefix + id : id;
+  _.uniqueId = function(prefix, moreUnique) {
+    var id = moreUnique ? ("" + Math.random()).slice(2) : idCounter++;
+    return !_.isUndefined(prefix) ? "" + prefix + id : id;
   };
 
   // By default, Underscore uses ERB-style template delimiters, change the
